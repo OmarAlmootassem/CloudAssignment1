56b957c7caf02a6190fea468f9bd5a41e89e1d6c
Omar Almootassem <218omar.a@gmail.com>
2017-01-22 18:19:31 -0500

    set up autocomplete search box

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,42 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope) {
-	$scope.text = "Hello";
+    $scope.selectedItem  = null;
+    $scope.searchText    = null;
+    $scope.querySearch   = querySearch;
+	$scope.data = [{
+		value: "omar",
+		display: "Omar"
+	},{
+		value: "amer",
+		display: "Amer"
+	},{
+		value: "omarr",
+		display: "Omarr"
+	},{
+		value: "amal",
+		display: "Amal"
+	},{
+		value: "ameer",
+		display: "Ameer"
+	}];
+
+	$scope.getInfo = function (query){
+		console.log("Getting info: " + query);
+	}
+
+	function querySearch (query) {
+      var results = query ? $scope.data.filter( createFilterFor(query) ) : $scope.data;
+      return results;
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+
 });
